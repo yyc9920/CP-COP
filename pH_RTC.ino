4d074d923a4c83000d7fa26808f7cc0b7baf368e
yyc9920 <53812519+yyc9920@users.noreply.github.com>
2019-11-19 00:57:19 +0900

    Update pH_RTC.ino

--- pH_RTC.ino
+++ pH_RTC.ino
@@ -26,59 +26,41 @@ int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
 int pHArrayIndex=0; 
 float pHValue,voltage;
 int droptime = 0;
+bool getDateflag = true;
+boolean GotString = false;
 
-void GetDateStuff(byte& Year, byte& Month, byte& Day, byte& DoW,
-        byte& Hour, byte& Minute, byte& Second) {
+void GetDateStuff() {
     // Call this if you notice something coming in on
     // the serial port. The stuff coming in should be in
     // the order YYMMDDwHHMMSS, with an 'x' at the end.
-    boolean GotString = false;
     char InChar;
     byte Temp1, Temp2, Temp3;
-    char InString[20];
- 
+    char InString[6];
     byte j=0;
-    while (!GotString) {
+    
+    if (Serial.available()) {
+    if (Serial.read() == 'a') {
+      getDateflag = false;
+    }
+    }
+    if (!getDateflag) {
+      while (!GotString){
         if (Serial.available()) {
             InChar = Serial.read();
             InString[j] = InChar;
             j += 1;
             if (InChar == 'x') {
                 GotString = true;
+                getDateflag = true;
             }
         }
+      }
+      Serial.println(InString);
     }
-    Serial.println(InString);
     // Read Year first
     Temp1 = (byte)InString[0] -48;
     Temp2 = (byte)InString[1] -48;
-    Year = Temp1*10 + Temp2;
-    // now month
-    Temp1 = (byte)InString[2] -48;
-    Temp2 = (byte)InString[3] -48;
-    Month = Temp1*10 + Temp2;
-    // now date
-    Temp1 = (byte)InString[4] -48;
-    Temp2 = (byte)InString[5] -48;
-    Day = Temp1*10 + Temp2;
-    // now Day of Week
-    DoW = (byte)InString[6] - 48;      
-    // now Hour
-    Temp1 = (byte)InString[7] -48;
-    Temp2 = (byte)InString[8] -48;
-    Hour = Temp1*10 + Temp2;
-    // now Minute
-    Temp1 = (byte)InString[9] -48;
-    Temp2 = (byte)InString[10] -48;
-    Minute = Temp1*10 + Temp2;
-    // now Second
-    Temp1 = (byte)InString[11] -48;
-    Temp2 = (byte)InString[12] -48;
-    Second = Temp1*10 + Temp2;
-
-    Temp1 = (byte)InString[13] -48;
-    Temp2 = (byte)InString[14] -48;
-    Temp3 = (byte)InString[15] -48;
+    Temp3 = (byte)InString[2] -48;
     k = Temp1*100 + Temp2*10 + Temp3;
 }
  
@@ -96,47 +78,11 @@ void setup() {
 void loop() {
   static unsigned long samplingTime = millis();
   static unsigned long printTime = millis();
-    int i;
+    int i = 0;
     // If something is coming in on the serial line, it's
     // a time correction so set the clock accordingly.
-    if (Serial.available()) {
-        GetDateStuff(Year, Month, Date, DoW, Hour, Minute, Second);
- 
-        Clock.setClockMode(false);  // set to 24h
-        //setClockMode(true);   // set to 12h
- 
-        Clock.setYear(Year);
-        Clock.setMonth(Month);
-        Clock.setDate(Date);
-        Clock.setDoW(DoW);
-        Clock.setHour(Hour);
-        Clock.setMinute(Minute);
-        Clock.setSecond(Second);
-         
-        // Give time at next five seconds
-        for (i=0; i<k; i++){
-            delay(1000);
-            Serial.print(Clock.getYear(), DEC);
-            Serial.print("-");
-            Serial.print(Clock.getMonth(Century), DEC);
-            Serial.print("-");
-            Serial.print(Clock.getDate(), DEC);
-            Serial.print(" ");
-            Serial.print(Clock.getHour(h12, PM), DEC); //24-hr
-            Serial.print(":");
-            Serial.print(Clock.getMinute(), DEC);
-            Serial.print(":");
-            Serial.println(Clock.getSecond(), DEC);
-            if(i == k-1){
-              Serial.println("feeding fish");
-              delay(1000);
-              Serial.println("end");
-              i = 0;
-            }
-        }
-    }
-
-   pHArray[pHArrayIndex++]=analogRead(SensorPin);
+  while(1){
+    pHArray[pHArrayIndex++]=analogRead(SensorPin);
    if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
    voltage = avergearray(pHArray, ArrayLenth)*5.0/1024; 
    pHValue = 3.5*voltage+Offset;
@@ -152,6 +98,7 @@ void loop() {
   delay(1000);
 
   droptime++;
+  i++;
 
   if(pHValue >= 8.0){
     if(droptime%5 == 0){  //용액 투여 후 pH 변화를 확인하는 여유 시간 5초를 두고 pH용액을 투여. 
@@ -166,6 +113,34 @@ void loop() {
     }
   }
   if(droptime>=10) droptime = 0;
+
+      GetDateStuff();
+      if (GotString) {
+ 
+        Clock.setClockMode(false);  // set to 24h
+        //setClockMode(true);   // set to 12h
+        GotString = false;
+        i = 0;
+      }
+        
+        Serial.print(Clock.getYear(), DEC);
+        Serial.print("-");
+        Serial.print(Clock.getMonth(Century), DEC);
+        Serial.print("-");
+        Serial.print(Clock.getDate(), DEC);
+        Serial.print(" ");
+        Serial.print(Clock.getHour(h12, PM), DEC); //24-hr
+        Serial.print(":");
+        Serial.print(Clock.getMinute(), DEC);
+        Serial.print(":");
+        Serial.println(Clock.getSecond(), DEC);
+        if(i == k){
+          Serial.println("feeding fish");
+          delay(1000);
+          Serial.println("end");
+          i = 0;
+        }
+  }
 }
 
 double avergearray(int* arr, int number)
